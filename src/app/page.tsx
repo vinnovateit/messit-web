f62ae6660ec7928725e6419e069e5486d85b51c9
manaslaud <manaslaud@hotmail.com>
2024-02-05 17:18:41 +0530

    feat:ui libs , calendar logic added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
+import getDates from "@/helpers/getDates";
 export default function Home() {
+  //tests
+  const today=new Date(2024,1,5)
+  const nextMonthSameDay=new Date(2024,2,5)
+  const arr:string[]=getDates(today,nextMonthSameDay)
+  const arrOfDates=
+  console.log(arr)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        
+        <Button variant={'secondary'}>
+          Hi there
+        </Button>
     </main>
   );
 }
---
+++ src/helpers/getDates.ts
@@ -0,0 +1,11 @@
+import moment from "moment";
+export default function getDates(startDate:Date, endDate:Date) {
+    let dateArray = [];
+    let currentDate = moment(startDate);
+    let stopDate = moment(endDate);
+    while (currentDate <= stopDate) {
+        dateArray.push( moment(currentDate).format('YYYY-MM-DD') )
+        currentDate = moment(currentDate).add(1, 'days');
+    }
+    return dateArray;
+}
