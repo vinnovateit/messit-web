18abab575584c8d3130bc891d9e5cb17a7db1cdd
thecoder-001 <ayush06feb@gmail.com>
2024-02-22 04:08:54 +0530

    Added mobile onboarding pages. Fixed issue related to async; would need to readd the deleted cookie handling code.
    Signed-off-by: thecoder-001 <ayush06feb@gmail.com>

---
+++ src/app/details/page.tsx
@@ -0,0 +1,39 @@
+'use client';
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
+import HostelSelection from "@/components/hostel-selection";
+import {Button} from "@/components/ui/button";
+import {useState,useEffect} from "react";
+
+export default function MessSelector(){
+    const [selectedHostel, setSelectedHostel] = useState(null);
+    const handleHostelSelect = (value) => {
+        setSelectedHostel(value);
+    };
+    useEffect(() => {
+        console.log("Mess stat (useEffect):", selectedHostel); // async troubles?
+    }, [selectedHostel]);
+    return (
+        <main className="flex min-h-dvh flex-col justify-between p-5">
+            <div className="text-4xl">First, we need to gather your <span className="text-[#53C0D3] font-bold">information</span></div>
+
+            <div className="text-2xl">This is required to find you the right mess menu</div>
+
+            <div className="text-lg font-semibold">Hostel Type <span className="text-[#53C0D3]">*</span></div>
+            <HostelSelection onSelect={handleHostelSelect}/>
+            <div className="text-lg font-semibold">Mess Type <span className="text-[#53C0D3]">*</span></div>
+            <Select>
+                <SelectTrigger className="w md:w-[300px] border-[#53C0D3]">
+                    <SelectValue placeholder="Select mess type"/>
+                </SelectTrigger>
+                <SelectContent>
+                    <SelectItem value="special">Special Mess</SelectItem>
+                    <SelectItem value="veg">Veg Mess</SelectItem>
+                    <SelectItem value="non-veg">Non-Veg mess</SelectItem>
+                </SelectContent>
+            </Select>
+            <Button variant={'default'} className="w-[213px] self-center">
+                Submit
+            </Button>
+        </main>
+    )
+}
---
+++ src/components/hostel-selection.tsx
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import ManEmoji from "@/public/man.png"
+import WomanEmoji from "@/public/woman.png"
+import Image from "next/image";
+
+const HostelSelection = ({ onSelect }) => {
+    const [selectedButton, setSelectedButton] = useState(null);
+
+    const handleButtonClick = (button) => {
+        setSelectedButton(button);
+        onSelect(button);
+    };
+    return (
+        <div className="flex self-center">
+            <button
+                className={`flex items-center justify-center border border-gray-300 rounded-lg p-2 m-2 focus:outline-none focus:bg-gray-200 hover:shadow-lg transform transition ${
+                    selectedButton === 'men' ? 'border-[#53C0D3] border-2 shadow-lg' : ''
+                }`}
+                onClick={() => handleButtonClick('men')}
+            >
+                <div className="flex flex-col">
+                    <Image src={ManEmoji} alt={"Men's hostel"} width={200}></Image>
+                    Men&apos;s Hostel
+                </div>
+            </button>
+            <button
+                className={`flex items-center justify-center border border-gray-300 rounded-lg p-2 m-2 focus:outline-none focus:bg-gray-200 hover:shadow-lg transform transition ${
+                    selectedButton === 'ladies' ? 'border-[#53C0D3] border-2 shadow-lg' : ''
+                }`}
+                onClick={() => handleButtonClick('ladies')}
+            >
+                <div className="flex flex-col">
+                    <Image src={WomanEmoji} alt={"Ladies' hostel"} width={200}></Image>
+                    Ladies&apos;s Hostel
+                </div>
+            </button>
+        </div>
+);
+};
+
+export default HostelSelection;
