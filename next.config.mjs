c2e8e82410249d21b509133a9857fadaace02170
thecoder-001 <ayush06feb@gmail.com>
2024-02-25 08:10:54 +0530

    Initial work on pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+import withSerwistInit from "@serwist/next";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+const withSerwist = withSerwistInit({
+    swSrc: "src/app/sw.ts",
+    swDest: "public/sw.js",
+});
+
+export default withSerwist({});
+// export default nextConfig;
