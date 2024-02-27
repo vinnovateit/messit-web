809b21523d79ffb56a4a23dc96e13823d16a684c
thecoder-001 <ayush06feb@gmail.com>
2024-02-28 04:45:56 +0530

    Added statusbar and ios tweaks Use apple-mobile-web-app-status-bar-style meta tag to set status bar color when changing theme.
    Signed-off-by: thecoder-001 <ayush06feb@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,13 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import {ThemeProvider} from "@/components/theme-provider";
+import Sidebar from "@/components/sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Messit Web",
-  description: "VinnovateIT",
+    title: "Messit Web",
+    description: "VinnovateIT",
 };
 
 export default function RootLayout({
@@ -17,16 +18,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
-      <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem
-          disableTransitionOnChange
-      >
+    <head>
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
+    </head>
+    <body className={inter.className}>
+    <ThemeProvider
+        attribute="class"
+        defaultTheme="light"
+        enableSystem
+        disableTransitionOnChange
+    >
+        <Sidebar/>
         {children}
-      </ThemeProvider>
-      </body>
+    </ThemeProvider>
+    </body>
     </html>
   );
 }
---
+++ src/components/sidebar.tsx
@@ -0,0 +1,61 @@
+'use client';
+import {Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger,} from "@/components/ui/sheet"
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
+import {HamburgerMenuIcon} from '@radix-ui/react-icons'
+import {useRouter} from 'next/navigation'
+import {useTheme} from "next-themes";
+import {Button} from "@/components/ui/button";
+import {usePathname} from 'next/navigation'
+
+const Sidebar = () => {
+    const pathname = usePathname()
+    const router = useRouter()
+    const {setTheme, theme} = useTheme();
+    const buttons = [
+        {label: "Messit", page: "/details"}, //todo: change after calendar added
+        {label: "Reset", page: "/"},
+        {label: "About us", page: "/about"}
+    ];
+    const handleButtonClick = (page: string) => {
+        console.log(`Button clicked for page: ${page}`);
+        router.push(page);
+    };
+    return (
+        <Sheet>
+            <SheetTrigger><HamburgerMenuIcon/></SheetTrigger>
+            <SheetContent side="left">
+                <SheetHeader>
+                    <SheetTitle>General</SheetTitle>
+                    {buttons.map((button, index) => (
+                        <Button
+                            key={index}
+                            variant="ghost"
+                            className={`w-full justify-start ${pathname === button.page ? "bg-slate-100 text-[#53C0D3] dark:text-[#98E4FF] dark:bg-slate-800" : "text-slate-500"}`}
+                            onClick={() => handleButtonClick(button.page)}
+                        >
+                            {button.label}
+                        </Button>
+                    ))}
+                    <SheetTitle>Preferences</SheetTitle>
+                    <Select onValueChange={(e) => setTheme(e)}>
+                        <SelectTrigger className="w">
+                            <SelectValue
+                                placeholder={theme ? theme[0].toUpperCase() + theme.slice(1) : "Select Theme"}/>
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectItem value="light">Light</SelectItem>
+                            <SelectItem value="dark">Dark</SelectItem>
+                            <SelectItem value="system">System</SelectItem>
+                        </SelectContent>
+                    </Select>
+                </SheetHeader>
+                <div
+                    className="absolute bottom-2 left-0 right-0 px-4 py-2 text-gray-900 dark:text-gray-100 text-sm text-center">Made
+                    with ❤️ by VinnovateIT
+                </div>
+            </SheetContent>
+        </Sheet>
+    )
+};
+
+export default Sidebar;
