ee428f3f9e3c385bf9d430151d09d5aa076e45fb
manaslaud <manaslaud@hotmail.com>
2024-03-02 20:21:56 +0530

    feat: calender, dates functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,12 @@
-import { Button } from "@/components/ui/button";
-import getDates from "@/helpers/getDates";
+'use client'
+import Calendar from "@/components/Calendar";
 import Link from 'next/link';
 
 export default function Home() {
-  //tests
-  const today=new Date(2024,1,5)
-  const nextMonthSameDay=new Date(2024,2,5)
-  const arr:string[]=getDates(today,nextMonthSameDay)
-  const arrOfDates=
-  console.log(arr)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <Link href={'/details'} >
-        <Button variant={'default'}>
-          Hi there
-        </Button>
+       
         </Link>
     </main>
   );
