4e27b22e2379254b01e68505c019261b971e5ec6
thecoder-001 <ayush06feb@gmail.com>
2024-04-08 21:53:42 +0530

    Initial work on date & menu carousel
    Revamped calendar and added theming
    Signed-off-by: thecoder-001 <ayush06feb@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,26 @@
 'use client'
 import Calendar from "@/components/Calendar";
 // import Card from "@/components/ui/Card";
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
 import MenuCard from "@/components/MenuCard";
 import {useEffect, useState} from "react";
 import Cookies from "js-cookie";
 import Sidebar from "@/components/sidebar";
 import LandingPage from "@/components/Landing";
+import {getDates} from "@/helpers/getDates";
 
 export default function Home() {
   const [showMainContent, setShowMainContent] = useState(false);
+  const [currentDateIndex, setCurrentDateIndex] = useState<number>(0);
+  const [dateArray, setDateArray] = useState<string[]>([]);
 
   useEffect(() => {
     const selectedHostel = Cookies.get('selectedHostelType');
@@ -17,11 +29,21 @@ export default function Home() {
     if (selectedHostel && selectedMessType) {
       setShowMainContent(true);
     }
+    const dates = getDates();
+    setDateArray(dates);
+    // set to current date
+    const today = new Date().toISOString().slice(0, 10);
+    const currentDateIndex = dates.findIndex(date => date === today);
+    setCurrentDateIndex(currentDateIndex);
   }, []);
 
   if (!showMainContent) {
     return <LandingPage/>;
   }
+
+  const handleDateSelect = (date: string) => {
+    console.log('Selected date:', date);
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-around laptop:p-16 gap-[2rem] mobile:p-8">
@@ -32,15 +54,42 @@ export default function Home() {
         <h1 className="w-full text-center mobile:text-[2rem] mobile:mt-[2rem]"> LH- <span className="font-bold text-[#53C0D3] dark:text-[#98E4FF]">Special Mess</span></h1>
         <h3 className="mobile:text-[2rem] mobile:w-full mobile:text-center"><b>March </b>2024</h3>
       </div>
-      <Calendar/>
-      <section className="grid laptop:grid-cols-2 justify-around items-center w-full gap-[2rem] flex-wrap mobile:grid-cols-1">
-        {/*todo: load data dynamically!!*/}
-        <MenuCard foodItems="Idli, Sambar, Chutney" meal="Breakfast" timing="7:00 AM - 9:00 AM" />
-        <MenuCard foodItems="Idli, Sambar, Chutney" meal="Breakfast" timing="7:00 AM - 9:00 AM" />
-        <MenuCard foodItems="Idli, Sambar, Chutney" meal="Breakfast" timing="7:00 AM - 9:00 AM" />
-        <MenuCard foodItems="Idli, Sambar, Chutney" meal="Breakfast" timing="7:00 AM - 9:00 AM" />
+      <Calendar onDateSelect={handleDateSelect} currentDateIndex={currentDateIndex} />
+      <Carousel className="w-full"
+                opts={{
+                  startIndex: currentDateIndex,
+                }}>
+        <CarouselContent>
+          {Array.from(dateArray).map((_, index) => (
+            <CarouselItem key={index}>
+              <div className="p-1">
+                {/*  <CardContent className="flex items-center justify-center p-6">*/}
+                {/*    <span className="text-4xl font-semibold flex items-center justify-center p-6">{index + 1}</span>*/}
+                    <section
+                      className="grid laptop:grid-cols-2 justify-around items-center w-full gap-[2rem] flex-wrap mobile:grid-cols-1">
+                      {/*todo: load data dynamically!!*/}
+                      <MenuCard foodItems="Idli, Sambar, Chutney" meal="Breakfast" timing="7:00 AM - 9:00 AM"/>
+                      <MenuCard foodItems="Idli, Sambar, Chutney" meal="Breakfast" timing="7:00 AM - 9:00 AM"/>
+                      <MenuCard foodItems="Idli, Sambar, Chutney" meal="Breakfast" timing="7:00 AM - 9:00 AM"/>
+                      <MenuCard foodItems="Idli, Sambar, Chutney" meal="Breakfast" timing="7:00 AM - 9:00 AM"/>
 
-      </section>
+                    </section>
+                  {/*</CardContent>*/}
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+
+      {/*<section*/}
+      {/*  className="grid laptop:grid-cols-2 justify-around items-center w-full gap-[2rem] flex-wrap mobile:grid-cols-1">*/}
+      {/*  /!*todo: load data dynamically!!*!/*/}
+      {/*  <MenuCard foodItems="Idli, Sambar, Chutney" meal="Breakfast" timing="7:00 AM - 9:00 AM"/>*/}
+      {/*  <MenuCard foodItems="Idli, Sambar, Chutney" meal="Breakfast" timing="7:00 AM - 9:00 AM"/>*/}
+      {/*  <MenuCard foodItems="Idli, Sambar, Chutney" meal="Breakfast" timing="7:00 AM - 9:00 AM" />*/}
+      {/*  <MenuCard foodItems="Idli, Sambar, Chutney" meal="Breakfast" timing="7:00 AM - 9:00 AM" />*/}
+
+      {/*</section>*/}
     </main>
   );
 }
--- src/helpers/getDates.ts
+++ src/helpers/getDates.ts
@@ -1,21 +1,21 @@
 import moment from "moment";
-export  function getDates() {
-    let startDate=moment().format('YYYY-MM-DD')
-    let endDate=moment().add(1,'M').format('YYYY-MM-DD')
+
+export function getDates() {
+    let startDate = moment().startOf('month').format('YYYY-MM-DD');
+    let endDate = moment().endOf('month').format('YYYY-MM-DD');
     let dateArray = [];
     let currentDate = moment(startDate);
     let stopDate = moment(endDate);
     while (currentDate <= stopDate) {
-        dateArray.push( (currentDate).format('YYYY-MM-DD') )
-        currentDate = (currentDate).add(1, 'days');
+        dateArray.push(currentDate.format('YYYY-MM-DD'));
+        currentDate = currentDate.add(1, 'days');
     }
     return dateArray;
 }
-export function getDays(dateArray:string[]){
-    let dayArray:string[]=[];
-dateArray.forEach((date)=>{
-    dayArray.push(moment(date).format('dddd'))
-})
-
-return dayArray
+export function getDays(dateArray: string[]) {
+    let dayArray: string[] = [];
+    dateArray.forEach((date) => {
+        dayArray.push(moment(date).format('ddd'));
+    });
+    return dayArray;
 }
