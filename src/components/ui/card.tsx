f2876342cb9cfbb5e9cc4949ec0f0732a6951ab4
adityadotexe <itsadi95@gmail.com>
2024-04-09 18:51:27 +0530

    fix

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,76 +1,21 @@
-import * as React from "react"
-
-import { cn } from "@/lib/utils"
-
-const Card = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn(
-      "rounded-xl border border-slate-200 bg-white text-slate-950 shadow dark:border-slate-800 dark:bg-slate-950 dark:text-slate-50",
-      className
-    )}
-    {...props}
-  />
-))
-Card.displayName = "Card"
-
-const CardHeader = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn("flex flex-col space-y-1.5 p-6", className)}
-    {...props}
-  />
-))
-CardHeader.displayName = "CardHeader"
-
-const CardTitle = React.forwardRef<
-  HTMLParagraphElement,
-  React.HTMLAttributes<HTMLHeadingElement>
->(({ className, ...props }, ref) => (
-  <h3
-    ref={ref}
-    className={cn("font-semibold leading-none tracking-tight", className)}
-    {...props}
-  />
-))
-CardTitle.displayName = "CardTitle"
-
-const CardDescription = React.forwardRef<
-  HTMLParagraphElement,
-  React.HTMLAttributes<HTMLParagraphElement>
->(({ className, ...props }, ref) => (
-  <p
-    ref={ref}
-    className={cn("text-sm text-slate-500 dark:text-slate-400", className)}
-    {...props}
-  />
-))
-CardDescription.displayName = "CardDescription"
-
-const CardContent = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
-))
-CardContent.displayName = "CardContent"
-
-const CardFooter = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn("flex items-center p-6 pt-0", className)}
-    {...props}
-  />
-))
-CardFooter.displayName = "CardFooter"
-
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
+import Image from "next/image"
+import React from "react";
+interface props{
+    foodItems:string[],
+    meal:string,
+    timing:string
+}
+const Card:React.FunctionComponent<props>=({foodItems,meal,timing})=>{
+    return (
+        <div className="flex flex-col justify-start gap-[2.5rem] py-[1rem] items-start px-[3rem]  relative overflow-hidden rounded-[1rem]  ">
+            <div className="absolute top-0 left-0 w-[20px] h-full bg-[#53C0D3]"></div>
+            <p className="font-bold text-[2.5rem]">Breakfast</p>
+            <p className="text-[1.5rem]">Masala dosa, sambar, chutney ducimus. </p>
+            <div className="text-[1.5rem] flex gap-[1rem]">
+                <Image width={32} height={32} src="/clock.svg" alt="Clock"/>
+                <p className="">Lorem ipsum dolor sit amet consectetur</p>
+             </div>
+        </div>
+    )
+}
+export default Card;
